0477d102fe15b29918b43abf4145f1b4ddf0e2be
Apfirebolt <aspper20@gmail.com>
2026-08-13 16:04:08 +0530

    feat: tailwind upgrade

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-import autoprefixer from 'autoprefixer';
-import tailwindcssPostcss from '@tailwindcss/postcss';
-
-export default {
-  plugins: [
-    tailwindcssPostcss,
-    autoprefixer,
-  ],
-}
